551e1382ff0566b8800093eeaa99a0113e1828ea
error404-vrs <ndomas@edenschool.fr>
2025-02-13 09:30:35 +0100

    q

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 const $gameCell = document.querySelector("#game-board");
 const $gameScreenTokens = document.querySelectorAll(".game-screen-token");
-const $gameBoardClass = document.querySelector(".game-board-class")
+const $gameBoardClass = document.querySelector(".game-board-class");
+const $pointer = document.querySelector(".rowPointer");
 
-const redToken = `<img src="./assets/counter-red-large.svg" alt="">`
-const yellowToken = `<img src="./assets/counter-yellow-large.svg" alt="">`
+const redToken = `<img src="./assets/counter-red-large.svg" alt="">`;
+const yellowToken = `<img src="./assets/counter-yellow-large.svg" alt="">`;
 
-let currentPlayer = 'red'
-
-const x = 0
-
+let currentPlayer = "red";
 
 let gameBoard = [
   ["", "", "", "", "", "", ""],
@@ -19,28 +17,49 @@ let gameBoard = [
   ["", "", "", "", "", "", ""],
 ];
 
+/**
+ *
+ */
 
-function clearBoard($completBoardDiv) {
-    location.reload();
+function clearBoard() {
+  location.reload();
 }
 
-$gameScreenTokens.forEach(function($gameScreenToken) {
-    $gameScreenToken.addEventListener("click", function() {
-        if (currentPlayer === "red") {
-            $gameScreenToken.innerHTML = redToken
-            currentPlayer = "yellow"
+$gameScreenTokens.forEach(function ($gameScreenToken) {
+  $gameScreenToken.addEventListener("click", function () {
+    const x = $gameScreenToken.getAttribute("data-x");
+
+    function dropToken(player) {
+      for (let i = 5; i >= 0; i--) {
+        if (gameBoard[i][x] === "") {
+          console.log("C'est vide");
+          document.querySelector(`
+            
+            [data-y="${i}"][data-x="${x}"]
+            
+            `).innerHTML = player;
+          break;
         } else {
-            $gameScreenToken.innerHTML = yellowToken
-            currentPlayer = "red"
+          console.log("C'est pas vide");
         }
-    })
-})
-
-
-for (let i = 5; i >= 0; i--) {
-    if (gameBoard[i][x] === "") {
-      console.log("C'est vide");
-    } else {
-      console.log("C'est pas vide");
+      }
     }
-  }
+    if (($gameScreenTokens.innerHTML = " ")) {
+      if (currentPlayer === "red") {
+        dropToken(redToken)
+        currentPlayer = "yellow";
+      } else {
+        dropToken(yellowToken)
+        currentPlayer = "red";
+      }
+    }
+  });
+});
+
+// $gameScreenTokens.forEach(function($gameScreenToken) {
+//     if (innerHTML = redToken) {
+//         $gameScreenToken.innerHTML = redToken
+//     } else if (innerHTML = yellowToken) {
+//         $gameScreenToken.innerHTML = yellowToken
+//     }
+// })
